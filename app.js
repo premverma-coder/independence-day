bea87d091ad45ccfcf87c414ae4ec7b8b4ea915c
premverma-coder <pv512681@gmail.com>
2024-09-02 23:55:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
   hh = h / 2,
 
   opts = {
-    strings: [  (I),(LOVE ),(INDIA)],
+    strings: ['I','LOVE ','INDIA'],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
